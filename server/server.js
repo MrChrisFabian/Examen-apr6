0bd2acd8b56c7dfcf4e3076c6d89b8aa0bd6ea15
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2024-04-07 13:53:11 -0400

    fix: variable env

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-const { PORT } = require("./config/settings");
 const express = require("express");
 const cors = require('cors')
 const app = express();
 const cookieParser = require('cookie-parser'); // to be able to read cookies
+
+require('dotenv').config();
 
 app.use(cookieParser());
 
@@ -13,8 +14,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require("./config/mongoose.config");
 
@@ -23,5 +24,7 @@ app.use("/api/movie", movieRoute);
 
 const UserRouter = require("./routes/user.routes");
 app.use("/api/auth", UserRouter);
+
+const PORT = process.env.PUERTO || 8000;
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
